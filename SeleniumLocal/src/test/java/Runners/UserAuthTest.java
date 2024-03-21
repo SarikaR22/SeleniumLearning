2684ee117c9c182130142b8d59547a5bfbff9692
Administrator <Administrator@172.31.5.239>
2024-03-21 07:27:04 +0000

    Commiting the file

---
+++ SeleniumLocal/src/test/java/Runners/UserAuthTest.java
@@ -0,0 +1,25 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src//test//resources//features//Login.feature"
+,glue = "stepDefs",
+monochrome=true,
+dryRun=false,
+plugin={"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class UserAuthTest extends AbstractTestNGCucumberTests {
+
+}
+/*
+ * features = "src//test//resources//features//UserAuthentication.feature"
+ * 
+ * ,glue = "stepDefinations"
+ * ,plugin = {"pretty", "usage:target\\UsageReport"
+ * "					html:target\\HTMLReport.html"
+ * 						"json:target\\JSONReport.json"
+ */
+
+
+//}
---
+++ SeleniumLocal/src/test/java/stepDefs/Login.java
@@ -0,0 +1,49 @@
+package stepDefs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login{
+	
+	WebDriver driver = new ChromeDriver();	
+
+@Given("I am on the login page")
+public void i_am_on_the_login_page(){
+	driver.get("https://www.saucedemo.com");
+    System.out.println("Login page loaded ");
+//    throw new io.cucumber.java.PendingException();
+}
+@When("I enter valid credentials")
+public void i_enter_valid_credentials() {
+	System.out.println("I am entering creds ");
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    // Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+}
+@When("I click the login button")
+public void i_click_the_login_button() {
+	driver.findElement(By.id("login-button")).click();
+	System.out.println("I clicked on login button ");
+	
+    // Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+}
+@Then("I should be redirected to the dashboard")
+public void i_should_be_redirected_to_the_dashboard() {
+	System.out.println("I am redirected to the  dashboard ");
+    // Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+}
+@Then("I should see a welcome message")
+public void i_should_see_a_welcome_message() {
+	System.out.println("I can see welcome message ");
+    // Write code here that turns the phrase above into concrete actions
+//  throw new io.cucumber.java.PendingException();
+}
+}
